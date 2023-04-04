0b7f47ea95e8b6308ada3b847ed5059fe5d30700
JiyaTeresa <jiyateresa@gmail.com>
2023-04-04 17:18:59 +0530

    first commit

---
+++ pgm1.sh
@@ -0,0 +1,12 @@
+echo "enter 2 numbers"
+read a
+read b
+s=`expr $a + $b`
+echo "sum is $s"
+d=`expr $a - $b`
+echo "diffrence is $d"
+p=`expr $a \* $b`
+echo "product is $p"
+d=`expr $a / $b`
+echo "division is $d"
+
